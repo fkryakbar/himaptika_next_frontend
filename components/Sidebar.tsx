0e74e22107ad44dda1cd8885565a283b4923dee7
fkrydev <fikri.afa289@gmail.com>
2023-04-24 22:07:45 +0800

    fix darkmode and fix searchbar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,7 +8,7 @@ function Sidebar() {
             <div className="w-full text-black">
                 <form action="/blog" method="GET" autoComplete="off">
                     <div className="input-group w-full">
-                        <input type="text" placeholder="Cari" name="search" className="input input-bordered" />
+                        <input type="text" placeholder="Cari" name="search" className="input input-bordered w-full" />
                         <button type="submit" className="btn btn-square bg-himaptika border-0 hover:bg-red-900">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,8 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: false,
+  },
   plugins: [require("daisyui")],
 };
